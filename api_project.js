30066ceb149197c8633b49d396a9fc5aa010dada
Jayson Grey <barinojayson@gmail.com>
2020-05-30 13:25:09 +0800

    activity material save and activity remove

--- api_project.js
+++ api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-05-29T16:09:02.871Z",
+    "time": "2020-05-30T05:20:26.350Z",
     "url": "http://apidocjs.com",
     "version": "0.22.1"
   }
